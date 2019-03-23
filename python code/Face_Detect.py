afc540dc6bf43358bbeb8463de1857d00b039e30
Nickiel <35903114+Nickiel12@users.noreply.github.com>
2019-03-23 13:41:32 -0700

    set Face_Detect.py to use a local webcam

--- python code/Face_Detect.py
+++ python code/Face_Detect.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-cap = cv2.VideoCapture("http://localhost:8080/test_mevo.webm" ,cv2.IMREAD_GRAYSCALE) # instead of grayscale you can also use -1, 0, or 1.
+cap = cv2.VideoCapture(1 ,cv2.IMREAD_GRAYSCALE) # instead of grayscale you can also use -1, 0, or 1.
 faceCascade = cv2.CascadeClassifier(r"C:\Python37-84-64\Lib\site-packages\cv2\data\haarcascade_frontalface_default.xml")
 
 #Color is 1, grayscale is 0, and the unchanged is -1
