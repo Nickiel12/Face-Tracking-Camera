dd88493aca39920a19f060a8f200bd129722b228
Nickiel <35903114+Nickiel12@users.noreply.github.com>
2019-03-31 13:57:04 -0700

    rewrote to have variable speed

--- Joystick Control/arduino.py
+++ Joystick Control/arduino.py
@@ -13,19 +13,20 @@ class Controller():
 
     class Servo():
         
-        upSlowStr = 'u'.encode('utf-8')
-        upFastStr = '1'.encode('utf-8')
+        upStr = bytes('u', 'utf8')
+        downStr = bytes('d', 'utf-8')
+        leftStr = bytes('l', 'utf-8')
+        rightStr = bytes('r', 'utf-8')
 
-        downSlowStr = 'd'.encode('utf-8')
-        downFastStr = '2'.encode('utf-8')
+        L1Tilt = bytes('1', 'utf-8')
+        L2Tilt = bytes('3', 'utf-8')
+        L3Tilt = bytes('5', 'utf-8')
+        L4Tilt = bytes('7', 'utf-8')
 
-        leftSlowStr = 'l'.encode('utf-8')
-        leftFastStr = '3'.encode('utf-8')
-
-        rightSlowStr = 'r'.encode('utf-8')
-        rightFastStr = '4'.encode('utf-8')
-
-        stopStr = 's'.encode('utf-8')
+        L1Turn = bytes('2', 'utf-8')
+        L2Turn = bytes('4', 'utf-8')
+        L3Turn = bytes('6', 'utf-8')
+        L4Turn = bytes('8', 'utf-8')
         
 
         def __init__(self):
@@ -33,31 +34,68 @@ class Controller():
         
         def up(self, level):
             assert type(level) == str, 'Not a string'
-            if level == 'LOW':
-                self.arduino.write(self.upSlowStr)
-            elif level == "HIGH":
-                self.arduino.write(self.upFastStr)
+            if level == '1':
+                self.arduino.write(self.L1Tilt)
+                self.arduino.write(self.upStr)
+            elif level == '2':
+                self.arduino.write(self.L2Tilt)
+                self.arduino.write(self.upStr)
+            elif level == '3':
+                self.arduino.write(self.L3Tilt)
+                self.arduino.write(self.upStr)
+            elif level == '4':
+                self.arduino.write(self.L4Tilt)
+                self.arduino.write(self.upStr)
+            else:
+                assert True == False, 'Unknown Level'
 
         def down(self, level):
             assert type(level) == str, 'Not a string'
-            if level == 'LOW':
-                self.arduino.write(self.downSlowStr)
-            elif level == "HIGH":
-                self.arduino.write(self.downFastStr)
+            if level == '1':
+                self.arduino.write(self.L1Tilt)
+                self.arduino.write(self.downStr)
+            elif level == '2':
+                self.arduino.write(self.L2Tilt)
+                self.arduino.write(self.downStr)
+            elif level == '3':
+                self.arduino.write(self.L3Tilt)
+                self.arduino.write(self.downStr)
+            elif level == '4':
+                self.arduino.write(self.L4Tilt)
+                self.arduino.write(self.downStr)
+            else:
+                assert True == False, 'Unknown Level'
 
         def right(self, level):
             assert type(level) == str, 'Not a string'
-            if level == 'LOW':
-                self.arduino.write(self.rightSlowStr)
-            elif level == "HIGH":
-                self.arduino.write(self.rightFastStr)
+            if level == '1':
+                self.arduino.write(self.L1Turn)
+                self.arduino.write(self.rightStr)
+            elif level == '2':
+                self.arduino.write(self.L2Turn)
+                self.arduino.write(self.rightStr)
+            elif level == '3':
+                self.arduino.write(self.L3Turn)
+                self.arduino.write(self.rightStr)
+            elif level == '4':
+                self.arduino.write(self.L4Turn)
+                self.arduino.write(self.rightStr)
+            else:
+                assert True == False, 'Unknown Level'
 
         def left(self, level):
             assert type(level) == str, 'Not a string'
-            if level == 'LOW':
-                self.arduino.write(self.leftSlowStr)
-            elif level == "HIGH":
-                self.arduino.write(self.leftFastStr)
-
-        def stop(self):
-            self.arduino.write(self.stopStr)
+            if level == '1':
+                self.arduino.write(self.L1Turn)
+                self.arduino.write(self.leftStr)
+            elif level == '2':
+                self.arduino.write(self.L2Turn)
+                self.arduino.write(self.leftStr)
+            elif level == '3':
+                self.arduino.write(self.L3Turn)
+                self.arduino.write(self.leftStr)
+            elif level == '4':
+                self.arduino.write(self.L4Turn)
+                self.arduino.write(self.leftStr)
+            else:
+                assert True == False, 'Unknown Level'
