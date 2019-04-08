dd5aadb117576a0949209b925aadb6c4aab0bca0
Nickiel <35903114+Nickiel12@users.noreply.github.com>
2019-04-07 18:07:32 -0700

    started testing with an esp8266 and wifi

---
+++ arduino code/ESP 8266/wifi test/wifi-test.ino
@@ -0,0 +1,25 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+
+const char* ssid = "Netgear1";
+const char* password = "a3aaf93071";
+
+void setup() {
+  pinMode(2, OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+}
+
+void loop() {
+
+  digitalWrite(2, !digitalRead(2));
+  delay(1000);
+}
