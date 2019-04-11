2841a562ab9c4167378f4d3419297b84b22a0109
Nickiel <35903114+Nickiel12@users.noreply.github.com>
2019-04-10 20:32:50 -0700

    removed irrelevant ESP files

--- arduino code/ESP 8266/wifi test/websites/SPIFFS test/SPIFFS_test/data/TemplateLoader.js
+++
@@ -1,6 +0,0 @@
-
-
-    var templatesImport = document.getElementById('MasterTemplate');
-    var templates = templatesImport.import;
-    var template = templates.getElementById('FullTemplate')
-    document.body.appendChild(document.importNode(template, true));
